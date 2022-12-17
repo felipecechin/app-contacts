9dcad5156d136b29b21d4937293bf3cf43c3b7d5
Felipe Cechin Mello <ficechin@hotmail.com>
2022-12-17 19:54:18 -0300

    fetcher added

---
+++ src/utils/fetcher.ts
@@ -0,0 +1,33 @@
+import FetchError from './FetchError'
+import env from '../env'
+
+interface IFetcherArgs {
+    method: string
+    url: string
+    data?: object
+    contentType?: string
+}
+
+const fetcher = async (args: IFetcherArgs): Promise<any> => {
+    const { method, url, data } = args
+    const headers: HeadersInit = {}
+
+    headers['Content-Type'] = 'application/json'
+    const pathUrl = `${env.API}${url}`
+
+    const response = await fetch(pathUrl, {
+        method,
+        headers,
+        body: data ? JSON.stringify(data) : undefined,
+    })
+
+    const responseData = await response.json()
+
+    if (!response.ok) {
+        throw new FetchError(response.status, response.statusText, responseData ? responseData : null)
+    }
+
+    return responseData
+}
+
+export default fetcher
