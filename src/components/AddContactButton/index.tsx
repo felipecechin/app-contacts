cc4ae2bbd8025381a7dc25c562c8b9db258ae14a
Felipe Cechin Mello <ficechin@hotmail.com>
2022-12-18 12:49:03 -0300

    added AddContactButton component

---
+++ src/components/AddContactButton/index.tsx
@@ -0,0 +1,18 @@
+import { Button } from './styles'
+import { FaPlus } from 'react-icons/fa'
+
+interface IAddContactButtonProps {
+    onClick: () => void
+}
+
+export default function AddContactButton({ onClick }: IAddContactButtonProps) {
+    return (
+        <Button
+            type='button'
+            onClick={onClick}
+        >
+            <FaPlus className='mr-2' />
+            Novo contato
+        </Button>
+    )
+}
