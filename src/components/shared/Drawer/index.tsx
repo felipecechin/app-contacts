e4e7b1083b4611553875a3230d6c86b2a1b549c8
Felipe Cechin Mello <ficechin@hotmail.com>
2022-12-18 12:50:48 -0300

    added Drawer

---
+++ src/components/shared/Drawer/index.tsx
@@ -0,0 +1,83 @@
+import {
+    Container,
+    Content,
+    ContentBody,
+    ContentTitle,
+    Overlay,
+    dialogPanelStyles,
+    dialogStyles,
+    dialogTitleStyles,
+} from './styles'
+import { Dialog, Transition } from '@headlessui/react'
+
+import { FaRegWindowClose } from 'react-icons/fa'
+import { Fragment } from 'react'
+
+interface IDrawerProps {
+    open: boolean
+    onClose: () => void
+    title: string
+    children: React.ReactNode
+}
+
+export default function Drawer({ open, onClose, title, children }: IDrawerProps): JSX.Element {
+    return (
+        <Transition.Root
+            as={Fragment}
+            show={open}
+        >
+            <Dialog
+                as='div'
+                onClose={onClose}
+                className={dialogStyles}
+            >
+                <Transition.Child
+                    as={Fragment}
+                    enter='ease-in-out duration-500'
+                    enterFrom='opacity-0'
+                    enterTo='opacity-100'
+                    leave='ease-in-out duration-500'
+                    leaveFrom='opacity-100'
+                    leaveTo='opacity-0'
+                >
+                    <Overlay />
+                </Transition.Child>
+                <Container>
+                    <div>
+                        <div>
+                            <Transition.Child
+                                as={Fragment}
+                                enter='transform transition ease-in-out duration-500'
+                                enterFrom='translate-y-full sm:translate-y-0 sm:translate-x-full'
+                                enterTo='translate-y-0 sm:translate-x-0'
+                                leave='transform transition ease-in-out duration-500'
+                                leaveFrom='translate-y-0 sm:translate-x-0'
+                                leaveTo='translate-y-full sm:translate-y-0 sm:translate-x-full'
+                            >
+                                <Dialog.Panel className={dialogPanelStyles}>
+                                    <Content>
+                                        <ContentTitle>
+                                            <Dialog.Title className={dialogTitleStyles}>{title}</Dialog.Title>
+                                            <button
+                                                onClick={onClose}
+                                                type='button'
+                                            >
+                                                <span>Close panel</span>
+                                                <FaRegWindowClose aria-hidden='true' />
+                                            </button>
+                                        </ContentTitle>
+                                        <ContentBody>
+                                            <div>
+                                                <div>{children}</div>
+                                            </div>
+                                        </ContentBody>
+                                    </Content>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </Container>
+            </Dialog>
+        </Transition.Root>
+    )
+}
---
+++ src/components/shared/Drawer/styles.ts
@@ -0,0 +1,53 @@
+import tw, { styled } from 'twin.macro'
+
+export const Overlay = styled.div`
+    ${tw`fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity`}
+`
+export const Container = styled.div`
+    ${tw`fixed inset-0 overflow-hidden`}
+
+    > div {
+        ${tw`absolute inset-0 overflow-hidden`}
+
+        > div {
+            ${tw`pointer-events-none fixed inset-y-0 right-0 flex max-w-full`}
+        }
+    }
+`
+
+export const dialogStyles = 'relative z-10'
+
+export const dialogPanelStyles = 'pointer-events-auto relative w-screen max-w-md'
+export const dialogTitleStyles = 'text-lg font-medium text-gray-900'
+
+export const Content = styled.div`
+    ${tw`flex h-full flex-col bg-white shadow-xl`}
+`
+
+export const ContentTitle = styled.div`
+    ${tw`w-full flex justify-between px-4 py-6 sm:px-6`}
+
+    > button {
+        ${tw`rounded-md text-gray-300 hover:text-cyan-700 focus:outline-none focus:ring-2 focus:ring-white`}
+
+        > span {
+            ${tw`sr-only`}
+        }
+
+        > svg {
+            ${tw`h-8 w-8`}
+        }
+    }
+`
+
+export const ContentBody = styled.div`
+    ${tw`relative mt-4 flex-1 px-4 sm:px-6`}
+
+    > div {
+        ${tw`absolute inset-0`}
+
+        > div {
+            ${tw`h-full border-gray-200`}
+        }
+    }
+`
