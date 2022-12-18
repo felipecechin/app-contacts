909fb674c24b35b26c3d16e729e5ac3304b3cd7d
Felipe Cechin Mello <ficechin@hotmail.com>
2022-12-18 19:17:52 -0300

    updated main

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import './index.css'
 
 import App from './App'
+import { ContactsProvider } from '@/contexts/useContacts'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <ContactsProvider>
+            <App />
+        </ContactsProvider>
     </React.StrictMode>
 )
