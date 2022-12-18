e15a69e5cda224c23ec3a955c8a8cd72aec931ee
Felipe Cechin Mello <ficechin@hotmail.com>
2022-12-18 10:51:52 -0300

    updated ContactItem styles

--- src/components/ContactItem/styles.ts
+++ src/components/ContactItem/styles.ts
@@ -1,7 +1,7 @@
 import tw, { styled } from 'twin.macro'
 
 export const Container = styled.li`
-    ${tw`flex items-center bg-white shadow rounded p-2 text-lg text-cyan-800`}
+    ${tw`flex items-center bg-white shadow rounded p-2 text-base sm:text-lg text-green-800`}
 
     > p {
         ${tw`flex-grow ml-2 font-semibold`}
@@ -13,9 +13,9 @@ export const Container = styled.li`
 `
 
 export const EditButton = styled.button`
-    ${tw`py-2 px-3 text-sm text-gray-900 bg-white rounded-l border border-gray-200 hover:bg-gray-100 hover:text-cyan-700 focus:z-10 focus:ring-2 focus:ring-cyan-700 focus:text-cyan-700`}
+    ${tw`py-2 px-3 text-sm text-gray-900 bg-white rounded-l border border-gray-200 hover:bg-gray-100 hover:text-green-700 focus:z-10 focus:ring-2 focus:ring-green-700 focus:text-green-700`}
 `
 
 export const DeleteButton = styled.button`
-    ${tw`py-2 px-3 text-sm text-gray-900 bg-white rounded-r border-t border-b border-r border-gray-200 hover:bg-gray-100 hover:text-cyan-700 focus:z-10 focus:ring-2 focus:ring-cyan-700 focus:text-cyan-700`}
+    ${tw`py-2 px-3 text-sm text-gray-900 bg-white rounded-r border-t border-b border-r border-gray-200 hover:bg-gray-100 hover:text-green-700 focus:z-10 focus:ring-2 focus:ring-green-700 focus:text-green-700`}
 `
